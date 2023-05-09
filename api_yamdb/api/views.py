b94c9ec4963ce0b44cdf1b9472f1bc78f8679e34
Кертиев Мурат <kertiev1107gmail.com>
2023-05-10 02:41:33 +0300

    Add 'models', 'view', 'url', 'serializers'

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
+from reviews.models import Title, Genre, Category
+from .serializer import TitleSerializer, GenreSerializer, CategorySerializer
+from rest_framework import mixins, viewsets
 
-# Create your views here.
+
+class TitleViewSet(ModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+
+
+class GenreViewSet(mixins.ListModelMixin,
+                   mixins.CreateModelMixin,
+                   mixins.DestroyModelMixin,
+                   viewsets.GenericViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+
+
+class CategoryViewSet(mixins.ListModelMixin,
+                      mixins.CreateModelMixin,
+                      mixins.DestroyModelMixin,
+                      viewsets.GenericViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,6 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'reviews',
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -85,7 +88,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 # Internationalization
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
